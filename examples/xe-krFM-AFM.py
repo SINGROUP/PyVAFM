128fa796b423093abab0b53008523c266a915b91
johnt447 <johntracey@Adams-MacBook-Pro.local>
2014-04-30 15:37:20 +0300

    added Am_AFM example

--- examples/xe-krFM-AFM.py
+++ examples/xe-krFM-AFM.py
@@ -1,136 +1,111 @@
-##!/usr/bin/env python
-import subprocess
-import sys
-sys.path.append('/Users/johntracey/Desktop/pyvafm-master/src')
-
-
-import vafmcircuits
+#!/usr/bin/env python
+from vafmcircuits import Machine
 from customs_pll import *
 
 
-from vafmbase import ChannelType
-from vafmcircuits import Machine
-
-
-
 def main():
-	
-	
-	machine = Machine(name='machine', dt=1e-7, pushed=True);
+
+	machine = Machine(machine=None, name='machine', dt=5e-8)
+
 	f0=23065
 	#Angstrom
 	Az=0.387
 
-	#Add Circuits
-
-	scanner = machine.AddCircuit(type='Scanner',name='scan', Process = machine, pushed=True)
-
-	#k=eV/ang^2
-	canti = machine.AddCircuit(type='Cantilever',name='canti', startingz=Az,
-		Q=42497, k=112.32, f0=f0, pushed=True)
-
-	machine.AddCircuit(type='delay', name='phi', DelayTime=0.5/f0, pushed=True)
-	machine.AddCircuit(type='Machine', name='ampd', assembly=aAMPD, fcut=500, pushed=True)
-	machine.AddCircuit(type='PI', name='agc', Kp=4, Ki=400, set=Az, pushed=True)
-
-	machine.AddCircuit(type='limiter', name='Forcelim', max=0.5, min=-0.5)
- 	machine.AddCircuit(type='limiter', name='exlim', min=0)
-
-	#####################################################################################
-	#dfpd setup
-
-	machine.AddCircuit(type='waver',name='test', amp=1, freq=f0, pushed=True)
-	machine.AddCircuit(type='waver',name='vco', amp=1, freq=f0, pushed=True)
-	machine.AddCircuit(type="Machine",name='pfd', assembly=dPFD, gain=600.0, fcut=500, KI=-300,KP=-0.3	, pushed=True)
-	machine.AddCircuit(type='opAdd',name='frq', in2=f0, pushed=True)	
-
-
-	#####################################################################################
-
-
-
-	machine.AddCircuit(type='opMul',name='exciter',pushed=True)
+	scanner = machine.AddCircuit(type='Scanner',name='scan', Process = machine, pushed=True)	
 	
+	canti = machine.AddCircuit(type='Cantilever',name='canti', 
+		Q=42497, k=112.32, f0=f0, startingz=Az, pushed=True)
+
+	machine.AddCircuit(type='waver',name='wave',freq=f0,amp=1)
+
+	machine.AddCircuit(type="Machine",name='amp', fcut=500, assembly=aAMPD, 
+		pushed=True)
+	
+	machine.AddCircuit(type="PI",name='agc', Ki=2.1, Kp=0.1, set=Az, pushed=True)
+	machine.AddCircuit(type="limiter",name='agclim', min=0,max=10, pushed=True)
+	
+	machine.AddCircuit(type="Machine",name='pll', assembly=dPFD, gain=1000.0, fcut=500, KI=3,KP=0.5, f0=f0	, pushed=True)
+	
+	machine.AddCircuit(type='opMul',name='pllinv',in2=-1, pushed=True)
+	machine.AddCircuit(type='opMul',name='exc', pushed=True)
+
+	###################################################################
+	#Force Setup
+	#xe-Kr
+	#dimer
+	machine.AddCircuit(type='VDWtorn', name='Dimer',A1=0.263191, A2=0.527478, A3=-1259.79, A4=23749.8, A5=-88463.2, A6=-4.00773, tipoffset=0, pushed=True)
+
+	#Surface
+	machine.AddCircuit(type='VDWtorn', name='Pyramid', A1=-1925.32, A2=635.529, A3=-1160.23, A4=22763.2, A5=-101664., A6=1.99542,tipoffset=0, pushed=True)
+ 
+	#Pyramid
+	machine.AddCircuit(type='VDWtorn', name='Surface', A1=-891.534, A2=416.779, A3=1558.35, A4=-8353.84, A5=15269.4, A6=-84.7053,tipoffset=0, pushed=True)
+	###################################################################
+
+	
+	machine.AddCircuit(type='phasor',name='pew', pushed=True)
 
 
 
 
-	machine.AddCircuit(type='VDWtorn', name='VDWtorn', A1=-230325, A2=29640.6 ,A3=289318,A4=-5.19433e7,A5=2.27228e9,tipoffset=0, pushed=True)
-	machine.AddCircuit(type='VDWtorn', name='TESTVDWtorn', A1=-230325, A2=29640.6 ,A3=289318,A4=-5.19433e7,A5=2.27228e9 ,tipoffset=0, pushed=True)
-
-
-	#Imaging output
-	imager = machine.AddCircuit(type='output',name='image',file='xe-kr-df.dat', dump=0)
-	imager.Register("scan.z","pfd.df")
-
-	#debug output
-	output = machine.AddCircuit(type='output',name='outputer',file='debug.dat', dump=100000)
-	output.Register("global.time","scan.z","pfd.df","canti.zabs","canti.ztip","canti.fz","ampd.amp","canti.exciter","TESTVDWtorn.fz")
-
-
-
-	#Give ztip to van der walls
-	machine.Connect('canti.ztip','ampd.signal')
-	machine.Connect('canti.zabs','VDWtorn.ztip')
-	machine.Connect('scan.z','TESTVDWtorn.ztip')
-
-
-    #feed z to cantilever then the ztip of canti to interpolation
 	machine.Connect("scan.z" , "canti.holderz")
-
-    #feed force to canti
-		
-
-	machine.Connect('canti.ztip','ampd.signal')
-	machine.Connect('ampd.amp','agc.signal')
-	
-	machine.Connect("ampd.norm","phi.signal")	
-
-
-
-	#####################################################################################
-	#dpfd connections
-	#machine.Connect("phi.out","pfd.ref")
-	machine.Connect("ampd.norm","pfd.ref")	
-	#machine.Connect("test.cos","pfd.ref")
-	machine.Connect("vco.sin","pfd.vco")
-	machine.Connect('pfd.df','frq.in1')
-	machine.Connect('frq.out','vco.freq')
-	
-	#####################################################################################
-
-	#Limiter 1
-	machine.Connect('agc.out','exlim.signal')
-	#machine.Connect('exlim.out','exciter.in1')
-	machine.Connect('agc.out','exciter.in1')
-
-
-	machine.Connect("VDWtorn.fz" , "Forcelim.signal")
-	#machine.Connect("Forcelim.out" , "canti.fz")
-	machine.Connect("VDWtorn.fz" , "canti.fz")
-
-	#####################################################################################
-	#Dpfd excitation
-
-	machine.Connect('vco.cos','exciter.in2')
-	machine.Connect('exciter.out','canti.exciter')
-
-	#####################################################################################
-
-
-	machine.Connect("scan.record","image.record")	
+	machine.Connect("canti.zabs" , "Dimer.ztip")
+	machine.Connect("Dimer.fz" , "canti.fz")
 	
 
 
-
-
+	machine.Connect('canti.ztip','amp.signal')
+	machine.Connect('amp.amp','agc.signal')
+	machine.Connect('amp.norm','pll.ref', 'pew.in1')
+	#machine.Connect('wave.cos','pll.ref')
+	machine.Connect('pll.cos','pll.vco','pew.in2')
 	
+	machine.Connect('agc.out','agclim.signal')
+	machine.Connect('agclim.out','exc.in1')
+	machine.Connect('pll.sin','pllinv.in1')
+	machine.Connect('pllinv.out','exc.in2')
+	
+	machine.Connect('exc.out','canti.exciter')
+	
+    #Outputs
+	out1 = machine.AddCircuit(type='output',name='output',file='testafm.out', dump=100)
+	out1.Register('global.time', 'canti.zabs','pll.ref','pll.cos','pll.sin','exc.in2','pll.dbg')
+	#out1.Register('global.time', 'wave.cos','pll.cos','pll.sin','exc.in2')
+	
+
+	out2 = machine.AddCircuit(type='output',name='output2',file='testafm2.out', dump=100)
+	out2.Register('global.time', 'amp.amp','agc.out','pll.df','pew.delay',"canti.fz")
+
+	out3 = machine.AddCircuit(type='output',name='output3',file='xe-kr-Dimer.out', dump=0)
+	out3.Register("scan.z", "pll.df")
+	out3.Stop()	
+
+
+	machine.Connect("scan.record","output3.record")	
+
+	out1.Stop()
+	out2.Stop()
+
 	scanner.Place(x=0,y=0,z=15)
 	scanner.Move(x=0,y=0,z=-1)	
 	machine.Wait(1)
 
-	scanner.MoveRecord(x=0,y=0,z=-5,v=1,points=200)
+	out3.Start()
+	scanner.MoveRecord(x=0,y=0,z=-9.5,v=1,points=200)
 
+
+
+'''
+	machine.Wait(0.01)
+	machine.circuits['wave'].I['freq'].Set(f0+14)
+	machine.Wait(0.01)
+	machine.circuits['wave'].I['freq'].Set(f0+20)
+	machine.Wait(0.01)
+'''
+
+
+	#plot testafm.out 1:3 (canti) 1:4 (pll reference) 1:6 (the exciter)
+	#u should see 3 distinct waves, canti peaks are in the middle between the other 2
 
 if __name__ == '__main__':
-	main()
+        main()
